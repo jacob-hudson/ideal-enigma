666fcc4ee0b61dc98b8dff5d5e3b404d4e1f7318
Jacob Hudson <jacob.alan.hudson@gmail.com>
2017-02-20 21:42:16 -0600

    Basic programmable infrastructure

--- config-sample.rb
+++ config-sample.rb
@@ -1,1 +1,21 @@
-#
+# Config 1 - Single VM,Generic Cluster,  Ubuntu 14.04
+$boxes = [
+  {
+    :cluster_size => 1,
+    :ram => "2048",
+    :os => "ubuntu",
+    :os_version => "14.04",
+    :ansible =>
+      [{ :group => "node", :roles => [ "*" ]}],
+    :forwarded_ports =>
+      { 80 => 8480,
+        443 => 8443,
+        3306 => 3306,
+        8080 => 8080,
+        9001 => 9001,
+        9002 => 9002,
+        9003 => 9003,
+        9004 => 9004
+      },
+  }
+]
