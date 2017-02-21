e460c22f05a0a3e64ea1555d82d25f723aecb972
Jacob Hudson <jacob.alan.hudson@gmail.com>
2017-02-20 21:58:37 -0600

    Added config 2 for larger memory hosts

--- config-sample.rb
+++ config-sample.rb
@@ -1,4 +1,4 @@
-# Config 1 - Single VM,Generic Cluster,  Ubuntu 14.04
+# Config 1 - Single VM, Generic Cluster,  Ubuntu 14.04
 $boxes = [
   {
     :cluster_size => 1,
@@ -10,12 +10,22 @@ $boxes = [
     :forwarded_ports =>
       { 80 => 8480,
         443 => 8443,
-        3306 => 3306,
-        8080 => 8080,
-        9001 => 9001,
-        9002 => 9002,
-        9003 => 9003,
-        9004 => 9004
+      },
+  }
+]
+
+# Config 2 - Multi VM, Generic Cluster,  Ubuntu 14.04
+$boxes = [
+  {
+    :cluster_size => 6,
+    :ram => "2048",
+    :os => "ubuntu",
+    :os_version => "14.04",
+    :ansible =>
+      [{ :group => "node", :roles => [ "*" ]}],
+    :forwarded_ports =>
+      { 80 => 8480,
+        443 => 8443,
       },
   }
 ]
