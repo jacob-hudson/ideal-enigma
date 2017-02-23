a76af2ade8a7d29ac304cac6b8d20aa026b7a3b8
Jacob Hudson <jacob.alan.hudson@gmail.com>
2017-02-22 20:48:35 -0600

    set default config

--- config-sample.rb
+++ config-sample.rb
@@ -15,17 +15,17 @@ $boxes = [
 ]
 
 # Config 2 - Multi VM, Generic Cluster,  Ubuntu 14.04
-$boxes = [
-  {
-    :cluster_size => 6,
-    :ram => "2048",
-    :os => "ubuntu",
-    :os_version => "14.04",
-    :ansible =>
-      [{ :group => "node", :roles => [ "*" ]}],
-    :forwarded_ports =>
-      { 80 => 8480,
-        443 => 8443,
-      },
-  }
-]
+# $boxes = [
+#   {
+#     :cluster_size => 6,
+#     :ram => "2048",
+#     :os => "ubuntu",
+#     :os_version => "14.04",
+#     :ansible =>
+#       [{ :group => "node", :roles => [ "*" ]}],
+#     :forwarded_ports =>
+#       { 80 => 8480,
+#         443 => 8443,
+#       },
+#   }
+# ]
